08ffa1687c0e7e55219cf7907eb87a4949d812bd
SebMZI <mrgy.sebastien@gmail.com>
2025-02-03 16:34:18 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,13 +4,20 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/fonts/**/*.{woff,woff2,ttf}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--text-color)",
+      },
+      fontFamily: {
+        akkurat: ["Akkurat", "Arial", "Helvetica", "sans-serif"],
+        stardom: ["Stardom", "sans-serif"],
+        amulya: ["Amulya", "sans-serif"],
       },
     },
   },
